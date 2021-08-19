4dd3469efd33e2c0fed86935a8a12f0a0141b6ec
Mike D Pilsbury <mike.pilsbury@gmail.com>
2021-08-19 16:23:56 +0100

    nextjs 8 - deploys succesfully

--- examples/app-basic/next.config.js
+++ examples/app-basic/next.config.js
@@ -2,7 +2,7 @@ const withCSS = require("@zeit/next-css");
 
 const config = {
   target: "serverless",
-  assetPrefix: "https://s3.amazonaws.com/mike-test-bkt",
+  assetPrefix: "https://s3.amazonaws.com/mike-test-bkt-2",
 };
 
 module.exports = withCSS(config);
