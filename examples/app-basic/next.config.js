81d09ac153ceba594b7c33d75cd72702758a5557
Mike D Pilsbury <mike.pilsbury@gmail.com>
2021-08-19 15:14:53 +0100

    Minimal changes. Just enough to try and deploy something. nextjs 8

--- examples/app-basic/next.config.js
+++ examples/app-basic/next.config.js
@@ -2,7 +2,7 @@ const withCSS = require("@zeit/next-css");
 
 const config = {
   target: "serverless",
-  assetPrefix: "https://s3.amazonaws.com/BUCKET_NAME"
+  assetPrefix: "https://s3.amazonaws.com/mike-test-bkt",
 };
 
 module.exports = withCSS(config);
