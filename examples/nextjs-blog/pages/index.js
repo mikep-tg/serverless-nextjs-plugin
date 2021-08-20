a2f2b0db3fd46185dd3f1a069fd71d1b2ebfefd0
Mike D Pilsbury <mike.pilsbury@gmail.com>
2021-08-20 08:23:15 +0100

    Make the home page a lambda

--- examples/nextjs-blog/pages/index.js
+++ examples/nextjs-blog/pages/index.js
@@ -1,4 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head";
+
+export const getServerSideProps = async () => {
+  return { props: {} };
+};
 
 export default function Home() {
   return (
@@ -54,8 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +208,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
